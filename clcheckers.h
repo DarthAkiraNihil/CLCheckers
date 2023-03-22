7f325021277df3c79c328e7f9ded78317c7f7d21
TheSwagVader <egorzverev666@gmail.com>
2023-03-22 20:31:48 +0300

    Added some things to finish later

--- clcheckers.h
+++ clcheckers.h
@@ -8,6 +8,8 @@
 enum BoardCellState {EMPTY_BLACK, EMPTY_WHITE, REG_BLACK, REG_WHITE, KING_BLACK, KING_WHITE};
 enum Color {Black, White};
 enum CheckerType {Regular, King};
+enum GameState {PlayerMove, EnemyMoveReal, EnemyMoveComputer};
+enum GameType {RvsR, RvsC}; //RvsR = Real versus Real, RvsC = Real vs Computer
 
 struct Coordinates {
     int x, y;
@@ -21,7 +23,7 @@ struct Checker {
 
 struct Move {
     Coordinates source, destination;
-    bool isKingMove, isTakingMove;
+    bool isKingMove, isTakingMove, isKingBecomingMove;
 };
 
 struct Board {
@@ -30,12 +32,27 @@ struct Board {
     int blacksCount, whitesCount;
     int rmCount, tmCount, kmCount;
     Move regularMoves[32], takingMoves[32], kingMoves[16];
+    //todo replace to another structure
 };
 
-void initiateChecker(Checker* checker, int x, int y) {
+struct GameSituation {
+    Board board;
+    int rmCount, tmCount, kmCount;
+    Color playerSide;
+    Move regularMoves[32], takingMoves[32], kingMoves[10];
+};
+
+struct Game {
+    GameSituation situation;
+    GameState state;
+    GameType type;
+};
+
+void initiateChecker(Checker* checker, int x, int y, Color color) {
     checker->coordinates.x = x;
     checker->coordinates.y = y;
-
+    checker->color = color;
+    checker->type = Regular;
 }
 
 void updateBoardRender(Board* board) {
@@ -54,39 +71,100 @@ void updateBoardRender(Board* board) {
     }
 }
 
-Board createANewBoard(Color playerSide) {
+Board createANewBoard(/*Color playerSide*/) {
     Board newBoard;
     newBoard.blacksCount = 12; newBoard.whitesCount = 12;
     newBoard.rmCount = 0; newBoard.tmCount = 0; newBoard.kmCount = 0;
-    for (int i = 0; i < 8; i++) {
+    /*for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             newBoard.boardRender[j][i] = ((i + j) % 2 == 0) ? EMPTY_BLACK : EMPTY_WHITE;
         }
-    }
+    }*/
     /*for (int i = 0; i < 12; i++) {
         initCPiece(&initBoard.blacks[i]);
         initCPiece(&initBoard.whites[i]);
     }*/
-    int valueOfInitedPairs = 0;
+    /*int valueOfInitedPairs = 0;
     for (short j = 0; j < 3; j++) {
         for (short k = 0; k < 8; k += 2) {
             if (playerSide == White) {
-                initiateChecker(&newBoard.whites[valueOfInitedPairs], k + (j % 2), j);
-                initiateChecker(&newBoard.blacks[valueOfInitedPairs++], k + 1 - (j % 2), 7 - j);
+                initiateChecker(&newBoard.whites[valueOfInitedPairs], k + (j % 2), j, White);
+                initiateChecker(&newBoard.blacks[valueOfInitedPairs++], k + 1 - (j % 2), 7 - j, Black);
             }
             else {
-                initiateChecker(&newBoard.blacks[valueOfInitedPairs], k + (j % 2), j);
-                initiateChecker(&newBoard.whites[valueOfInitedPairs++], k + 1 - (j % 2), 7 - j);
+                initiateChecker(&newBoard.blacks[valueOfInitedPairs], k + (j % 2), j, Black);
+                initiateChecker(&newBoard.whites[valueOfInitedPairs++], k + 1 - (j % 2), 7 - j, White);
             }
         }
+    }*/
+    int valueOfInitedPairs = 0;
+    for (short j = 0; j < 3; j++) {
+        for (short k = 0; k < 8; k += 2) {
+            initiateChecker(&newBoard.whites[valueOfInitedPairs], k + (j % 2), j, White);
+            initiateChecker(&newBoard.blacks[valueOfInitedPairs++], k + 1 - (j % 2), 7 - j, Black);
+        }
     }
-    //}
     updateBoardRender(&newBoard);
     //for (int i = 0)
     return newBoard;
 }
 
-void findAllRegularMoves(Board* board, Checker checker, Color playerSide, Move* arrayTo) {
+GameSituation makeNullGameSituation(Color playerSide) {
+    GameSituation gameSituation;
+    gameSituation.board = createANewBoard();
+    gameSituation.playerSide = playerSide;
+    gameSituation.rmCount = 0; gameSituation.tmCount = 0; gameSituation.kmCount = 0;
+}
+
+Game createANewGame(Color playerSide, Color firstMove, GameType type) {
+    Game newGame;
+    newGame.situation = makeNullGameSituation(playerSide);
+    newGame.state = (playerSide == firstMove) ? PlayerMove : ((type == RvsR) ? EnemyMoveReal : EnemyMoveComputer);
+}
+
+void findAllRegularMoves(GameSituation* situation, Color forWhichSide) {
+    int foundMoves;
+    if (forWhichSide == Black) {
+        //for (int i )
+    }
+    else if (forWhichSide == White) {
+        for (int i = 0; i < situation->board.whitesCount; i++) {
+            Move move;
+            move.isKingMove = false; move.isTakingMove = false;
+            int ex = situation->board.whites[i].coordinates.x;
+            int ey = situation->board.whites[i].coordinates.y;
+            if (ey < 7) {
+                switch (ex) {
+                    case 0: {
+                        if ((situation->board.boardRender[ey+1][ex+1] == REG_BLACK) || (situation->board.boardRender[ey+1][ex+1] == KING_BLACK)) {
+                            Move move;
+                            move.source.y = ey; move.source.x = ex;
+                            move.destination.y = ey + 1; move.destination.x = ex + 1;
+                        }
+                        break;
+                    }
+                    case 7: {
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    /*else if (forWhichSide == White) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (situation->board.boardRender[j][i] == REG_WHITE) {
+
+                }
+            }
+        }
+    }*/
+}
+
+/*cvoid findAllRegularMoves(Board* board, Checker checker, Color playerSide, Move* arrayTo) {
 }
 
 void findAllTakingMoves(Board* board, Checker checker, Move* arrayTo) {
@@ -115,7 +193,7 @@ void findAllMoves(Board* board, Color forWho, Color playerSide) {
     else if (forWho == Black && playerSide == Black) {
 
     }
-}
+}/*
 
 void removeChecker(Board* board, int index, Color color) {
     if (color == Black && index < board->blacksCount) {
@@ -131,9 +209,7 @@ void removeChecker(Board* board, int index, Color color) {
         board->whitesCount--;
     }
 }
+*/
 
-struct Game {
-
-};
 #endif CHECKERS_CLCHECKERS_H
 
